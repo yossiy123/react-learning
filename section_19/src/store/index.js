bc95aad306eb671c1f4d2dcaead607f915564587
Yossi Yanay <yossiy11@gmail.com>
2023-10-27 12:21:44 +0300

    init store

---
+++ section_19/src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux';
+
+
+const defaultState = {
+    counter: 0,
+};
+
+const counterReducer = (state=defaultState, action) => {
+    if (action === 'increment')
+    {
+        return {
+            counter: state.counter + 1,
+        };
+    }
+    if (action === 'decrement')
+    {
+        return {
+            counter: state.counter - 1,
+        };
+    }
+
+    return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
